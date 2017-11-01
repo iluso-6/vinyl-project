6b22ea35960fbdd2af42d9415e4cc9d40b5ec9e5
Shay de Barra <x16115864@student.ncirl.ie>
2017-11-01 23:26:25 +0000

    changed background scss and head

--- vinylstore/config/environments/production.rb
+++ vinylstore/config/environments/production.rb
@@ -9,7 +9,17 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+# Disable Rails's static asset server (Apache or nginx will already do this)  
+config.serve_static_assets = false
 
+# Compress JavaScripts and CSS  
+config.assets.compress = true
+
+# Don't fallback to assets pipeline if a precompiled asset is missed  
+config.assets.compile = false
+
+# Generate digests for assets URLs  
+config.assets.digest = true
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -28,7 +38,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
